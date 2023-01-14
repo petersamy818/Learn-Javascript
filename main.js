f2930237f1ec867f20c2193b6b11e77725a541b2
Peter Samy <peterpop828@gmail.com>
2023-01-15 00:31:53 +0200

    Day 1 Learning JS

---
+++ main.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    // document.querySelector("h1").style.color = "blue";
+    // window.alert("Hello From JS File");
+    // document.write("<h1>Hello <span>Page</Span></h1>");
+    // console.log("Hello From %cJS %cFile", "color: red; Font-size: 40px", "color: blue; Font-size: 40px");
+
+};
