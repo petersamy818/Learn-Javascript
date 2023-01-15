283e65021c8d21c9c3512f54f2f301e59d5e9ab3
Peter Samy <peterpop828@gmail.com>
2023-01-16 00:25:31 +0200

    Day 2 Learning JS

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
 window.onload = function () {
+
+    // classes from 1 - 9
+
     // document.querySelector("h1").style.color = "blue";
     // window.alert("Hello From JS File");
     // document.write("<h1>Hello <span>Page</Span></h1>");
     // console.log("Hello From %cJS %cFile", "color: red; Font-size: 40px", "color: blue; Font-size: 40px");
 
+
+    // assignments Solution
+
+    // document.querySelector("h1").style.fontWeight = "bold";
+    // document.querySelector("h1").style.fontSize = "80px";
+    // document.querySelector("h1").style.fontFamily = "arial";
+    // document.querySelector("h1").style.color = "blue";
+    // document.querySelector("h1").style.textAlign = "center";
+
+    // console.log("%cElzero %cWeb %cSchool", "color: red; font-size: 40px;", "color: green; font-size: 40px; font-weight: bold", "background-color: blue; color: white; font-size: 40px;");
+
+    // console.group("Group 1");
+    // console.log("Message 1");
+    // console.log("Message 2");
+    // console.group("Child Group");
+    // console.log("Message 1");
+    // console.log("Message 2");
+    // console.group("Grand Child Group");
+    // console.log("Message 1");
+    // console.log("Message 2");
+    // console.groupEnd();
+    // console.groupEnd();
+    // console.groupEnd();
+    // console.group("Group 2");
+    // console.log("Message 1");
+    // console.log("Message 2");
+
+    // console.table(["Elzero", "Ahmed", "Sameh", "Gamal", "Aya"]);
+
+    // classes from 10 - 17
+
+    // var user = "peter",
+    //     age = 37;
+    // console.log(user);
+    // console.log(age);
+    // console.log(hello);
+    // hello.innerHTML = "Option";
+
+    
 };
